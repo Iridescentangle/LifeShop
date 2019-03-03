421ad42f409a83955294ab13fead6b4f2dbeeb5c
Iridescentangle <17611420051@163.com>
2019-03-03 22:58:11 +0800

    店长头像 电话拨打功能  商品推荐

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:life_shop/service/service_method.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:convert';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:url_launcher/url_launcher.dart';
 class HomePage extends StatefulWidget {
   final Widget child;
 
@@ -26,11 +27,20 @@ class _HomePageState extends State<HomePage> {
             var data = json.decode(snapshot.data);
             List<Map> swiperData = (data['data']['slides'] as List).cast();
             List navigatorList = (data['data']['category'] as List).cast();
-            return Column(
-              children: <Widget>[
-                SwiperDiy(swiperDataList: swiperData,),
-                GridNavigator(navigatorList: navigatorList,),
-              ],
+            String adPicture = data['data']['advertesPicture']['PICTURE_ADDRESS'];
+            String leaderImg = data['data']['shopInfo']['leaderImage'];
+            String leaderPhone = data['data']['shopInfo']['leaderPhone'];
+            List recommendList = data['data']['recommend'];
+            return SingleChildScrollView(
+              child:Column(
+                children: <Widget>[
+                  SwiperDiy(swiperDataList: swiperData,),
+                  GridNavigator(navigatorList: navigatorList,),
+                  ADBanner(adPicture: adPicture,),
+                  LeaderPhone(picUrl: leaderImg,phone: leaderPhone,),
+                  Recommend(recommendList: recommendList,),
+                ],
+              ),
             );
           }else{
             return Text('加载中',style: TextStyle(fontSize: ScreenUtil().setSp(28)),);
@@ -87,6 +97,121 @@ class GridNavigator extends StatelessWidget {
             padding: EdgeInsets.all(5.0),
             children: navigatorList.map((item)=>_gridViewItem(context, item)).toList(),
       )
+    );
+  }
+}
+class ADBanner extends StatelessWidget {
+  final String adPicture;
+
+  ADBanner({Key key, this.adPicture}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.network(adPicture),
+    );
+  }
+}
+class LeaderPhone extends StatelessWidget {
+  final String picUrl;
+  final String phone;
+
+  LeaderPhone({Key key, this.picUrl,this.phone}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: InkWell(
+        onTap:_launchUrl,
+        child: Image.network(picUrl),
+      ),
+    );
+  }
+  void _launchUrl() async{
+    String url = 'tel:'+phone;
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+}
+class Recommend extends StatelessWidget {
+  final List recommendList;
+  Recommend({Key key, this.recommendList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ScreenUtil.instance.setHeight(380),
+      margin: EdgeInsets.only(top: 10.0),
+      child: Column(
+        children: <Widget>[
+          _titleWidget(),
+          _recommendList(),
+        ],
+      ),
+    );
+  }
+  //标题
+  Widget _titleWidget(){
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.fromLTRB(10.0, 2.0, 0, 5.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(width: 0.5,color: Colors.black12),
+        ),
+      ),
+      child: Text('商品推荐',style:TextStyle(color:Colors.pink)),
+    );
+  }
+  //横向列表
+  Widget _recommendList(){
+    return Container(
+      height: ScreenUtil.instance.setHeight(330),
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: recommendList.length,
+        itemBuilder: _item,
+      ),
+    );
+  }
+  //图片单独项方法
+  Widget _item(BuildContext context,int index){
+    return InkWell(
+      onTap: (){
+
+      },
+      child: Container(
+        height: ScreenUtil.instance.setHeight(330),
+        width: ScreenUtil.instance.setWidth(250),
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            left: BorderSide(
+              width: 0.5,color: Colors.black12,
+            ),
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+            Image.network(recommendList[index]['image']),
+            Text(
+              '￥${recommendList[index]['mallPrice']}',
+              ),
+            Text(
+              '￥${recommendList[index]['price']}',
+              style: TextStyle(
+                decoration: TextDecoration.lineThrough,
+                color: Colors.grey,
+              ),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
