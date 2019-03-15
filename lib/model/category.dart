5f68ee9ab14b47b55378c260d13ae54bdaae7444
Iridescentangle <17611420051@163.com>
2019-03-15 17:31:33 +0800

    111

--- lib/model/category.dart
+++ lib/model/category.dart
@@ -1,34 +1,94 @@
-class CategoryModel{
-  String mallCategoryId;
+class CategoryModel {
+  String code;
+  String message;
+  List<CategoryListModel> data;
 
-  String mallCategoryName;
-  Null comments;
-  List bxMallSubDto;
-  String image;
-  CategoryModel({
-    this.bxMallSubDto,
-    this.comments,
-    this.image,
-    this.mallCategoryId,
-    this.mallCategoryName,
+  CategoryModel({this.code, this.message, this.data});
+
+  CategoryModel.fromJson(Map<String, dynamic> json) {
+    code = json['code'];
+    message = json['message'];
+    if (json['data'] != null) {
+      data = new List<CategoryListModel>();
+      json['data'].forEach((v) {
+        data.add(new CategoryListModel.fromJson(v));
+      });
     }
-  );
-  factory CategoryModel.fromJson(dynamic json){
-    return CategoryModel(
-      bxMallSubDto: json['bxMallSubDto'],
-      comments: json['comments'],
-      image: json['image'],
-      mallCategoryId: json['mallCategoryId'],
-      mallCategoryName: json['mallCategoryName'],
-    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['code'] = this.code;
+    data['message'] = this.message;
+    if (this.data != null) {
+      data['data'] = this.data.map((v) => v.toJson()).toList();
+    }
+    return data;
   }
 }
-class CategoryListModel{
-  List<CategoryModel> data;
-  CategoryListModel({this.data});
-  factory CategoryListModel.fromJson(List json){
-    return CategoryListModel(
-      data:json.map((i)=>CategoryModel.fromJson(i)).toList(),
-    );
+
+class CategoryListModel {
+  String mallCategoryId;
+  String mallCategoryName;
+  List<BxMallSubDto> bxMallSubDto;
+  Null comments;
+  String image;
+
+  CategoryListModel(
+      {this.mallCategoryId,
+      this.mallCategoryName,
+      this.bxMallSubDto,
+      this.comments,
+      this.image});
+
+  CategoryListModel.fromJson(Map<String, dynamic> json) {
+    mallCategoryId = json['mallCategoryId'];
+    mallCategoryName = json['mallCategoryName'];
+    if (json['bxMallSubDto'] != null) {
+      bxMallSubDto = new List<BxMallSubDto>();
+      json['bxMallSubDto'].forEach((v) {
+        bxMallSubDto.add(new BxMallSubDto.fromJson(v));
+      });
+    }
+    comments = json['comments'];
+    image = json['image'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['mallCategoryId'] = this.mallCategoryId;
+    data['mallCategoryName'] = this.mallCategoryName;
+    if (this.bxMallSubDto != null) {
+      data['bxMallSubDto'] = this.bxMallSubDto.map((v) => v.toJson()).toList();
+    }
+    data['comments'] = this.comments;
+    data['image'] = this.image;
+    return data;
+  }
+}
+
+class BxMallSubDto {
+  String mallSubId;
+  String mallCategoryId;
+  String mallSubName;
+  String comments;
+
+  BxMallSubDto(
+      {this.mallSubId, this.mallCategoryId, this.mallSubName, this.comments});
+
+  BxMallSubDto.fromJson(Map<String, dynamic> json) {
+    mallSubId = json['mallSubId'];
+    mallCategoryId = json['mallCategoryId'];
+    mallSubName = json['mallSubName'];
+    comments = json['comments'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['mallSubId'] = this.mallSubId;
+    data['mallCategoryId'] = this.mallCategoryId;
+    data['mallSubName'] = this.mallSubName;
+    data['comments'] = this.comments;
+    return data;
   }
 }
--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -4,6 +4,7 @@ import '../service/service_method.dart';
 import '../config/service_url.dart';
 import 'dart:convert';
 import '../model/category.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 class CategoryPage extends StatefulWidget {
   _CategoryPageState createState() => _CategoryPageState();
 }
@@ -14,28 +15,81 @@ class _CategoryPageState extends State<CategoryPage> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    request('category').then((result){
-      var data = (json.decode(result))['data'];
-      CategoryListModel list = CategoryListModel.fromJson(data);
-      for (var item in list.data){
-        print(item.mallCategoryName);
-      }
-      setState(() {
-        text = result;
-      });
-    });
   }
   @override
   Widget build(BuildContext context) {
-//    return FutureBuilder(
-//      future: (),
-//      builder: (context,snapshot){
-//        return Container();
-//      },
-//    );
-    return Center(
-      child: Text(text),
+    return Scaffold(
+      appBar: AppBar(title: Text('商品分类'),),
+      body: Container(
+        child: Row(
+          children: <Widget>[
+            LeftCategoryNavigator(),
+          ],
+        ),
+      ),
     );
 
+  }
+}
+class LeftCategoryNavigator extends StatefulWidget {
+  @override
+  _LeftCategoryNavigatorState createState() => _LeftCategoryNavigatorState();
+}
+
+class _LeftCategoryNavigatorState extends State<LeftCategoryNavigator> {
+   List list = [];
+   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getCategory();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: ScreenUtil.instance.setWidth(180),
+      decoration: BoxDecoration(
+        border: Border(
+          right: BorderSide(
+            width: 1,
+            color: Colors.black12
+          ),
+        ),
+      ),
+      child: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context,index){
+          return _leftInkWell(index);
+        },
+      ),
+    );
+  }
+  void _getCategory() async{
+    await request('category').then((result){
+      var data = json.decode(result.toString());
+      CategoryModel model =CategoryModel.fromJson(data);
+      model.data.forEach((item)=>print(item.mallCategoryName));
+      setState(() {
+        list = model.data;
+      });
+    });
+  }
+  Widget _leftInkWell(int index) {
+    return InkWell(
+      onTap: (){
+
+      },
+      child: Container(
+        height: ScreenUtil.instance.setHeight(100),
+        padding: EdgeInsets.only(left: 10.0,top: 20.0,),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom:BorderSide(width: 1,color: Colors.black12),
+          ),
+        ),
+        child: Text(list[index].mallCategoryName,style: TextStyle(fontSize: 26),),
+      ),
+    );
   }
 }
